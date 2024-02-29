94e85cd4f09c498ddc12a82f110637915adc5793
TeslaBoy <teslaboy79@gmail.com>
2024-02-29 21:26:33 +0200

    Was fixed dropdown

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,6 +1,6 @@
 import {IoChevronDownSharp} from "react-icons/io5";
 import DropdownList from "./DropdownList"
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useRef} from "react";
 
 const Dropdown = (props) => {
   const [isElementOn, setIsElementOn] = useState(false);
@@ -8,11 +8,16 @@ const Dropdown = (props) => {
   const toggleElement = () => {
     setIsElementOn(prevState => !prevState); 
   };
+  let menuRef = useRef();
 
   useEffect(()=>{
     document.addEventListener("mousedown", 
-      ()=> {setIsElementOn(false);
-    })
+      (e)=> {
+        if (menuRef.current)
+          if (!menuRef.current.contains(e.target))
+            setIsElementOn(false);
+          // console.log(menuRef.current);
+        })
   });
 
     function handleClick() {
@@ -20,7 +25,7 @@ const Dropdown = (props) => {
     }
     return(   
         <div>
-          <div className="" >
+          <div className="">
             <button onClick={handleClick} className="dropdown-button">
               <label className="hidden md:block w-32">Categories</label> 
               <IoChevronDownSharp className="w-6 h-4mt-1"/>
@@ -28,7 +33,7 @@ const Dropdown = (props) => {
           </div>
           {
             isElementOn && 
-            <div className='dropdown-list'>
+            <div className='dropdown-list' ref={menuRef}>
               <DropdownList onChangeFilter={props.onChangeFilter} state = {props.state}/> 
             </div>
           }
